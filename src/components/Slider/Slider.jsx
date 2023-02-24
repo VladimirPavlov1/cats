7bf4c5806d93a8ed30381c47c169d61df06b8c82
VladimirPavlov1 <pavvvlovvvvvvladimir@gmail.com>
2023-02-24 13:48:53 +0200

    start cats

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,14 @@
+import { useState,useEffect, createContext } from "react";
+import PropTypes from 'prop-types';
+
+
+
+const Slider=({width,height,autoPlay,autoPlayTime})=>{
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Slider;
